63518a71eb29557f4a6098a164c7fa2528f574e2
Razvan <razvanrujoi@yahoo.com>
2022-08-01 22:21:27 +0300

    create and resolve exercice 05_show_args

---
+++ 05_show_args/src/main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <iostream>
+#include <string>
+#include <ctype.h>
+#include <locale>
+
+
+int main(int argc, char* argv[])
+{
+	if (argc <= 1)
+	{
+		printf("No program arguments found.\n");
+		return 0;
+	}
+	for (int i = 1; i < argc; i++) {
+		std::string argumentAsString = argv[i];
+	
+		std::cout << argumentAsString << std::endl;
+	}
+	
+	return 0;
+}
